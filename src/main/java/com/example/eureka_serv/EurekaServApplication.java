01b0687ded44ed61d02d69214bed956ce187f6e1
stayonasDev <stay_on_track@naver.com>
2025-10-13 10:56:09 +0900

    init

---
+++ src/main/java/com/example/eureka_serv/EurekaServApplication.java
@@ -0,0 +1,13 @@
+package com.example.eureka_serv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eureka_serv/EurekaServApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureka_serv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
