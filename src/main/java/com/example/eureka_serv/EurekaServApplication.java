9d002313c0471326c08cc3d2799a43529b3568c3
stayonasDev <stay_on_track@naver.com>
2025-10-21 14:29:12 +0900

    형상관리 문제로 애플리케이션에 다시 @EnableEurekaServer 명시

--- src/main/java/com/example/eureka_serv/EurekaServApplication.java
+++ src/main/java/com/example/eureka_serv/EurekaServApplication.java
@@ -2,8 +2,10 @@ package com.example.eureka_serv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServApplication {
 
 	public static void main(String[] args) {
